3d70a83f133d114ad17ce90c6fbc1284fd786e4c
minhminion <minhminion2015@gmail.com>
2020-06-18 17:53:42 +0700

    [FEATURE] Get Currency Rate Form RSS

--- app/src/main/java/com/example/currencyconvert/MainActivity.java
+++ app/src/main/java/com/example/currencyconvert/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -21,6 +23,8 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
     Spinner selectToCurrency;
     EditText inputCurrency;
     Button btnChangeCurrency;
-    private double currencyRate = 1;
+    TextView mResult;
+    String fromCurrency;
+    String toCurrency;
+    double currencyRate = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
         selectFromCurrency = (Spinner) findViewById(R.id.from_currencies_spinner);
         selectToCurrency = (Spinner) findViewById(R.id.to_currencies_spinner);
 
+        inputCurrency = (EditText) findViewById(R.id.inputCurrency) ;
         btnChangeCurrency = (Button) findViewById(R.id.btnChangeCurrency);
+
+        mResult = (TextView) findViewById(R.id.mResult);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -50,11 +60,25 @@ public class MainActivity extends AppCompatActivity {
         selectFromCurrency.setAdapter(adapter);
         selectToCurrency.setAdapter(adapter);
 
+        selectFromCurrency.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                fromCurrency = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         selectToCurrency.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("Selected", parent.getItemAtPosition(position).toString());
+                toCurrency = parent.getItemAtPosition(position).toString();
+
             }
 
             @Override
@@ -75,28 +99,31 @@ public class MainActivity extends AppCompatActivity {
     private class FetchFeedTask extends AsyncTask<Void, Void, Boolean> {
         private String urlLink;
 
-
         @Override
-        protected void onPreExecute() {
-            urlLink = "https://mxn.fxexchangerate.com/eur.xml";
+        protected void onPreExecute () {
+//            mSwipeLayout.setRefreshing(true);
+            urlLink = "https://"+fromCurrency.toLowerCase()+".fxexchangerate.com/"+toCurrency.toLowerCase()+".xml";
         }
-
         @Override
-        protected Boolean doInBackground(Void... voids) {
+        protected Boolean doInBackground(Void ... voids) {
 //            if("", e);Toast.makeText(gTextUtils.isEmpty(urlLink)){
 //                return false;
 //            }
             try {
-                if (!urlLink.startsWith("http://") &&
+                if(!urlLink.startsWith("http://") &&
                         !urlLink.startsWith("https://"))
                     urlLink = "http://" + urlLink;
                 URL url = new URL(urlLink);
                 InputStream inputStream = url.openConnection().getInputStream();
-                currencyRate = parseFeed(inputStream);
+                RssFeedModel mFeedModel = parseFeed(inputStream);
+                String rate = TextUtils.split(mFeedModel.description, "<br/>")[0];
+                rate = TextUtils.split(rate, "=")[1];
+                rate = TextUtils.split(rate, " ")[1];
+                currencyRate = Double.parseDouble(rate);
                 return true;
             } catch (IOException e) {
                 Log.e("Error", e.toString());
-
+//                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
             } catch (XmlPullParserException e) {
 //                   Log.e(TAG, "ErroretApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
             }
@@ -104,14 +131,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Boolean success) {
+        protected  void onPostExecute (Boolean success) {
+//            mSwipeLayout.setRefreshing(false);
 
-            if (success) {
-//                mFeedTitleTextView.setText("Feed Title: " + mFeedTitle);
-//                mFeedDescriptionTextView.setText("Feed Description: " + mFeedDescription);
-//                mFeedLinkTextView.setText("Feed Link: " + mFeedLink);
-//                // Fill RecyclerView
-//                mRecyclerView.setAdapter(new RssFeedListAdapter(getApplicationContext(), mFeedModelList));
+            if(success) {
+                Double currency = Double.parseDouble(inputCurrency.getText().toString());
+                Double result = currency*currencyRate;
+                Log.i("Result", result.toString());
             } else {
                 Toast.makeText(MainActivity.this,
                         "Enter a valid Rss feed url",
@@ -120,54 +146,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public double parseFeed(InputStream inputStream) throws XmlPullParserException,
+    public RssFeedModel parseFeed(InputStream inputStream) throws XmlPullParserException,
             IOException {
         String title = null;
         String link = null;
         String description = null;
+        RssFeedModel item = null;
         boolean isItem = false;
-        double currencyRate = 0;
         try {
             XmlPullParser xmlPullParser = Xml.newPullParser();
             xmlPullParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             xmlPullParser.setInput(inputStream, null);
 //            xmlPullParser.nextTag();
-            Log.i("xml", xmlPullParser.toString());
             while (xmlPullParser.next() != XmlPullParser.END_DOCUMENT) {
                 int eventType = xmlPullParser.getEventType();
                 String name = xmlPullParser.getName();
                 if (name == null)
                     continue;
-                if (eventType == XmlPullParser.END_TAG) {
-                    if (name.equalsIgnoreCase("item")) {
+                if(eventType == XmlPullParser.END_TAG) {
+                    if(name.equalsIgnoreCase("item")) {
                         isItem = false;
                     }
                     continue;
                 }
                 if (eventType == XmlPullParser.START_TAG) {
-                    if (name.equalsIgnoreCase("item")) {
+                    if(name.equalsIgnoreCase("item")) {
                         isItem = true;
                         continue;
                     }
                 }
                 Log.d("MyXmlParser", "Parsing name ==> " + name);
                 String result = "";
-                if (xmlPullParser.next() == XmlPullParser.TEXT) {
+                if (xmlPullParser.next() == XmlPullParser.TEXT){
                     result = xmlPullParser.getText();
                     xmlPullParser.nextTag();
                 }
-                if (name.equalsIgnoreCase("title")) {
+                if (name.equalsIgnoreCase("title")){
                     title = result;
                 } else if (name.equalsIgnoreCase("link")) {
                     link = result;
                 } else if (name.equalsIgnoreCase("description")) {
                     description = result;
                 }
-                return currencyRate;
-            }
+
+                if (title != null && link != null && description != null) {
+                    if(isItem) {
+                        item = new RssFeedModel(title, link, description);
+                    }
+                    title = null;
+                    link = null;
+                    description = null;
+                    isItem = false;
+                }}
+            return item;
         } finally {
             inputStream.close();
-        }
-        return 1;
-    }
+        }}
 }
